8c809daadf6854e493d91c0857d16f14ceb36994
Daniele122898 <d.palbanese@gmail.com>
2018-11-11 18:49:55 +0100

    refactored and started work on actual app

--- Indecision-app/public/scripts/app.js
+++ Indecision-app/public/scripts/app.js
@@ -23,53 +23,5 @@ var template = React.createElement(
     )
 );
 
-var count = 0;
-
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-
-var subOne = function subOne() {
-    count--;
-    renderCounterApp();
-};
-
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
-};
-
 var appRoot = document.getElementById("app");
-
-var renderCounterApp = function renderCounterApp() {
-    var template2 = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Count: ',
-            count
-        ),
-        React.createElement(
-            'button',
-            { onClick: addOne },
-            '+1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: subOne },
-            '-1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: reset },
-            'reset'
-        )
-    );
-
-    ReactDOM.render(template2, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);
---
+++ Indecision-app/src/playground/counter-example.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+const addOne = () => {
+    count++;
+    renderCounterApp();
+};
+
+const subOne = () => {
+    count--;
+    renderCounterApp();
+};
+
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
+
+const appRoot = document.getElementById("app");
+
+const renderCounterApp = () => {
+    const template2 = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={subOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+
+    ReactDOM.render(template2, appRoot);
+};
+
+renderCounterApp();
