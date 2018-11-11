33ea1446fe3383d52c49f782fba131c3fd11a823
Daniele122898 <d.palbanese@gmail.com>
2018-11-11 19:00:42 +0100

    added some logic for adding options and rerendering

--- Indecision-app/public/scripts/app.js
+++ Indecision-app/public/scripts/app.js
@@ -4,24 +4,57 @@ console.log("App.js is running");
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'This is a noice app'
+    subtitle: 'This is a noice app',
+    options: []
 };
 
-// JSX - JavaScript XML
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.subtitle
-    )
-);
+var onFormSubmit = function onFormSubmit(event) {
+    // stop full page refresh
+    event.preventDefault();
+
+    var option = event.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        event.target.elements.option.value = "";
+        renderApp();
+    }
+};
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+var renderApp = function renderApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
--- Indecision-app/src/app.js
+++ Indecision-app/src/app.js
@@ -2,16 +2,40 @@ console.log("App.js is running");
 
 const app = {
     title: 'Indecision App',
-    subtitle: 'This is a noice app'
+    subtitle: 'This is a noice app',
+    options: []
 };
 
-// JSX - JavaScript XML
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
-    </div>
-);
+const onFormSubmit = (event) => {
+    // stop full page refresh
+    event.preventDefault();
+
+    const option = event.target.elements.option.value;
+
+    if(option) {
+        app.options.push(option);
+        event.target.elements.option.value = "";
+        renderApp();
+    }
+};
+
 
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            <p>{app.subtitle}</p>
+            <p>{app.options.length}</p>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
