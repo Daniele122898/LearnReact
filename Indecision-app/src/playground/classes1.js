ad692803a40d1e118d1b54624cb4f24c1765872e
Daniele122898 <d.palbanese@gmail.com>
2018-11-12 22:12:14 +0100

    can also override and call super

--- Indecision-app/src/playground/classes1.js
+++ Indecision-app/src/playground/classes1.js
@@ -15,10 +15,23 @@ class Person {
 }
 
 class Student extends Person{
-
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let desc = super.getDescription();
+        if (this.hasMajor()) {
+            desc += ` Their major is ${this.major}.`;
+        }
+        return desc;
+    }
 }
 
-const me = new Student("Serenity", 20);
+const me = new Student("Serenity", 20, "Computer Science");
 console.log(me.getDescription());
 
 const other = new Student();
