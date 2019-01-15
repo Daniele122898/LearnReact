130d87c4d3944f8c3747539267a9520f7fce1484
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2019-01-15 22:13:35 +0100

    Some state and changing and children modifying some stuff

--- Indecision-app/src/playground/counter-example.js
+++ Indecision-app/src/playground/counter-example.js
@@ -1,33 +1,48 @@
-let count = 0;
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0
+    };
+  }
 
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count+1
+      };
+    });
+  }
 
-const subOne = () => {
-    count--;
-    renderCounterApp();
-};
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count-1
+      };
+    });
+  }
 
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
+  handleReset() {
+    this.setState((prevState) => {
+      return {
+        count: 0
+      };
+    });
+  }
 
-const appRoot = document.getElementById("app");
-
-const renderCounterApp = () => {
-    const template2 = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={subOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
     );
+  }
+}
 
-    ReactDOM.render(template2, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(<Counter />, document.getElementById('app'));
--- Indecision-app/src/playground/visibility-toggle.js
+++ Indecision-app/src/playground/visibility-toggle.js
@@ -1,29 +1,64 @@
-const data = {
-    visible: false,
-    visText: "Show details",
-    invisText: "Hide details",
-    textToShow: "Some text that is hidden"
-};
+class Visibility extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggle = this.handleToggle.bind(this);
+        this.data = {
+            visText: "Show details",
+            invisText: "Hide details",
+            textToShow: "Some text that is hidden"
+        };
+        this.state = {
+            visibility: false
+        };
+    }
 
-const toggleHide = () => {
-    data.visible = !data.visible;
-    render();
-};
+    handleToggle() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
 
-const appRoot = document.getElementById("app");
+    render() {
+        return (
+          <div>
+              <h1>Visibility Toggle</h1>
+              <button onClick={this.handleToggle}>{this.state.visibility ? this.data.invisText : this.data.visText}</button>
+              { this.state.visibility && (<p>{this.data.textToShow}</p>) }
+          </div>
+        );
+    }
+}
 
-const render = () => {
-    const app = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleHide}>{data.visible ? data.invisText : data.visText}</button>
-            {data.visible && (
-                <p>{data.textToShow}</p>
-            )}
-        </div>
-    );
+ReactDOM.render(<Visibility />, document.getElementById('app'));
 
-    ReactDOM.render(app, appRoot);
-};
-
-render();
+// const data = {
+//     visible: false,
+//     visText: "Show details",
+//     invisText: "Hide details",
+//     textToShow: "Some text that is hidden"
+// };
+//
+// const toggleHide = () => {
+//     data.visible = !data.visible;
+//     render();
+// };
+//
+// const appRoot = document.getElementById("app");
+//
+// const render = () => {
+//     const app = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleHide}>{data.visible ? data.invisText : data.visText}</button>
+//             {data.visible && (
+//                 <p>{data.textToShow}</p>
+//             )}
+//         </div>
+//     );
+//
+//     ReactDOM.render(app, appRoot);
+// };
+//
+// render();
