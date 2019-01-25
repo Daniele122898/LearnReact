04808f732ccdceb08697a69581b3ff9056f25715
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2019-01-25 23:43:09 +0100

    some things

--- Expensify-app/src/playground/redux-expensify.js
+++ Expensify-app/src/playground/redux-expensify.js
@@ -1,9 +1,21 @@
 import { createStore, combineReducers } from 'redux';
 import uuid from 'uuid';
+// REMOVE_EXPENSE
+const removeExpense = (id) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
 
 // ADD_EXPENSE
 const addExpense = (
-  {
+{
     description = '',
     note = '',
     amount = 0,
@@ -20,11 +32,30 @@ const addExpense = (
   }
 });
 
+// TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
     case 'ADD_EXPENSE':
       return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id}) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense)=>{
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
@@ -38,6 +69,11 @@ const filterReducerDefaultState = {
 };
 const filterReducer = (state = filterReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
     default:
       return state;
   }
@@ -55,8 +91,14 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(addExpense({ description: "rent", amount: 100 }));
-store.dispatch(addExpense({ description: "some other stuff", amount: 300 }));
+const exp1 = store.dispatch(addExpense({ description: "rent", amount: 100 }));
+const exp2 = store.dispatch(addExpense({ description: "COFEFEEEE", amount: 300 }));
+
+store.dispatch(removeExpense(exp1.expense.id));
+
+store.dispatch(editExpense(exp2.expense.id, { amount: 23 }));
+
+store.dispatch(setTextFilter('rent'));
 
 const demoState = {
   expenses: [{
@@ -72,4 +114,9 @@ const demoState = {
     startDate: undefined,
     endDate: undefined
   }
+};
+
+const user = {
+  name: 'Jen',
+  age: 24
 };
