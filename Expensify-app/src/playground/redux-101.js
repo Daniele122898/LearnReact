ff7d8d6c08a7a631bd4cbcd083d31d72ac9813e8
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2019-01-25 22:10:41 +0100

    added redux and started with store,actions and subscriptions

---
+++ Expensify-app/src/playground/redux-101.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux';
+
+const store = createStore((state = { count: 0 }, action)=> {
+  switch (action.type) {
+    case ('INCREMENT'):
+      return {
+        count: state.count+1
+      };
+    case ('DECREMENT'):
+      return {
+        count: state.count-1
+      };
+    case ('RESET'):
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+const unsubscribe = store.subscribe(()=>{
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+//unsubscribe();
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
