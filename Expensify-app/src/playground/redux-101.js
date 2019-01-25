24383a7144814dbadbd7b2c6954bd107cc051696
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2019-01-25 22:49:33 +0100

    action generators

--- Expensify-app/src/playground/redux-101.js
+++ Expensify-app/src/playground/redux-101.js
@@ -1,15 +1,25 @@
 import { createStore } from 'redux';
 
+// Action generators
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
 const store = createStore((state = { count: 0 }, action)=> {
   switch (action.type) {
     case ('INCREMENT'):
       return {
-        count: state.count+(action.incrementBy ? action.incrementBy : 1)
-        //count: state.count+(action.incrementBy === 'number' ? action.incrementBy:1)
+        count: state.count+action.incrementBy
       };
     case ('DECREMENT'):
       return {
-        count: state.count-1
+        count: state.count-action.decrementBy
       };
     case ('RESET'):
       return {
@@ -24,19 +34,13 @@ const unsubscribe = store.subscribe(()=>{
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 //unsubscribe();
 store.dispatch({
   type: 'RESET'
 });
 
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(decrementCount({decrementBy: 10}));
+store.dispatch(decrementCount());
