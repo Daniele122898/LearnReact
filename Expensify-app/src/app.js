9ea7a8123230de6c5184b81df216065fe29d5d49
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2019-01-27 22:20:58 +0100

    added Higher order components HCO

--- Expensify-app/src/app.js
+++ Expensify-app/src/app.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
-import configureStore from './store/configureStore'
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-console.log(store.getState());
+// addExpense -> Water bill
+store.dispatch(addExpense({description: 'water bill', amount: 1000, createdAt: 10}));
+// addExpense -> gas bill
+store.dispatch(addExpense({description: 'gas bill', amount: 500, createdAt: 100}));
+// setTextFilter -> bill --> water
+store.dispatch(setTextFilter("bill"));
+console.log(getVisibleExpenses(store.getState().expenses,store.getState().filter));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
