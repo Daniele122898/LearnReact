5dbf65691941ee596dfcda978ccab8ad5a3d695d
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2019-01-31 21:39:31 +0100

    made webpack ready for production

--- Expensify-app/src/app.js
+++ Expensify-app/src/app.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({description: 'water bill', amount: 1000, createdAt: 10}));
-store.dispatch(addExpense({description: 'gas bill', amount: 500, createdAt: 100}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-
-console.log(getVisibleExpenses(store.getState().expenses,store.getState().filter));
 
 const jsx = (
   <Provider store={store}>
