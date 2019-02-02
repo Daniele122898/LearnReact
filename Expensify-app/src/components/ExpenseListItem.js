5e132aeac219fed39f9bed372e8f144e525f3eb8
DANIELE-XPS15\danie <d.palbanese@gmail.com>
2019-02-02 22:20:21 +0100

    made numerals into CHF

--- Expensify-app/src/components/ExpenseListItem.js
+++ Expensify-app/src/components/ExpenseListItem.js
@@ -2,6 +2,25 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
+// load a locale
+numeral.register('locale', 'CH', {
+  delimiters: {
+    thousands: "'",
+    decimal: '.'
+  },
+  abbreviations: {
+    thousand: 'k',
+    million: 'm',
+    billion: 'b',
+    trillion: 't'
+  },
+  currency: {
+    symbol: 'CHF'
+  }
+});
+
+// switch between locales
+numeral.locale('CH');
 
 const ExpenseListItem =  ({description, amount, createdAt, id}) => (
   <div>
@@ -9,7 +28,7 @@ const ExpenseListItem =  ({description, amount, createdAt, id}) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {numeral(amount/100).format('$0,0.00')}
+      {numeral(amount/100).format("$0,0.00")}
       -
       {moment(createdAt).format('MMMM Do, YYYY')}
     </p>
