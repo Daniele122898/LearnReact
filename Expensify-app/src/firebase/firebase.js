7e8371f89a19ff9ea8d475bf296c52aab21eb84e
Daniele122898 <d.palbanese@gmail.com>
2019-02-04 20:22:28 +0100

    firebase playaround

---
+++ Expensify-app/src/firebase/firebase.js
@@ -0,0 +1,54 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBi-Gm93TRlrpulBGUBhPdsrXfXKbA8ch8",
+  authDomain: "expensify-9449c.firebaseapp.com",
+  databaseURL: "https://expensify-9449c.firebaseio.com",
+  projectId: "expensify-9449c",
+  storageBucket: "expensify-9449c.appspot.com",
+  messagingSenderId: "886645230633"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+const onValueChange = database.ref().on('value', (snapshot) => {
+  console.log(snapshot.val());
+});
+
+setTimeout(()=> {
+  database.ref().off('value', onValueChange);
+}, 7000);
+
+/*
+database.ref()
+  .once('value')
+  .then((snapshot)=> {
+    const data = snapshot.val();
+    console.log(data);
+  })
+  .catch((e)=> {
+    console.log('Error fetching data',e)
+  });
+*/
+/*
+firebase.database().ref().set({
+  name: 'Andrew Mead',
+  age: 26,
+  job: 'Software developer',
+  location: {
+    city: 'Philly',
+    country: 'US'
+  }
+}).then(()=> {
+  console.log('Saved successfully');
+}).catch((e)=> {
+  console.log("Notn't'0tnttn",e);
+});
+
+firebase.database().ref().update({
+  job: 'Manager',
+  'location/city': 'Boston'
+});
+*/
