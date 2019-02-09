bbcf2434583e8f536a03702ce217b7d987fd9342
Daniele122898 <d.palbanese@gmail.com>
2019-02-09 22:27:19 +0100

    Added all the necessary styles

--- Expensify-app/src/components/ExpenseListFilters.js
+++ Expensify-app/src/components/ExpenseListFilters.js
@@ -19,34 +19,45 @@ class ExpenseListFilters extends React.Component {
 
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.props.filters.text}
-          onChange={(e)=> {
-            // dispatch action to store
-            this.props.dispatch(setTextFilter(e.target.value));
-          }}
-        />
-        <select
-          value={this.props.filters.sortBy}
-          onChange={(e)=> {
-            this.props.dispatch(e.target.value === "date" ? sortByDate() : sortByAmount());
-          }}
-        >
-          <option value={"date"}>Date</option>
-          <option value={"amount"}>Amount</option>
-        </select>
-        <DateRangePicker
-          startDate={this.props.filters.startDate}
-          endDate={this.props.filters.endDate}
-          onDatesChange={this.onDatesChange}
-          focusedInput={this.state.calendarFocused}
-          onFocusChange={this.onFocusChange}
-          showClearDates={true}
-          numberOfMonths={1}
-          isOutsideRange={()=> false }
-        />
+      <div className="content-container">
+        <div className="input-group">
+          <div className="input-group__item">
+            <input
+              placeholder="Search expenses"
+              className="text-input"
+              type="text"
+              value={this.props.filters.text}
+              onChange={(e)=> {
+                // dispatch action to store
+                this.props.dispatch(setTextFilter(e.target.value));
+              }}
+            />
+          </div>
+          <div className="input-group__item">
+            <select
+              className="select"
+              value={this.props.filters.sortBy}
+              onChange={(e)=> {
+                this.props.dispatch(e.target.value === "date" ? sortByDate() : sortByAmount());
+              }}
+            >
+              <option value={"date"}>Date</option>
+              <option value={"amount"}>Amount</option>
+            </select>
+          </div>
+          <div className="input-group__item">
+            <DateRangePicker
+              startDate={this.props.filters.startDate}
+              endDate={this.props.filters.endDate}
+              onDatesChange={this.onDatesChange}
+              focusedInput={this.state.calendarFocused}
+              onFocusChange={this.onFocusChange}
+              showClearDates={true}
+              numberOfMonths={1}
+              isOutsideRange={()=> false }
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- Expensify-app/src/components/ExpenseSummary.js
+++ Expensify-app/src/components/ExpenseSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import expenseTotal from "../selectors/expenses-total";
@@ -10,8 +11,13 @@ const ExpenseSummary = ({expenseCount, expensesTotal}) => {
     const expw = expenseCount === 1 ? "expense" : "expenses";
     const expensesTotalFormatted = numeral(expensesTotal/100).format("$0,0.00");
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expw} totalling {expensesTotalFormatted}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expw} totalling <span>{expensesTotalFormatted}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
