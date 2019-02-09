294d7ad05e7aeb34640463a3f5e432a27de32da2
Daniele122898 <d.palbanese@gmail.com>
2019-02-09 18:10:16 +0100

    added authentication and private routes

--- Expensify-app/src/app.js
+++ Expensify-app/src/app.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import numeral from 'numeral';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from "./actions/expenses";
+import { login, logout } from "./actions/auth";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -35,17 +36,30 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
-
-store.dispatch(startSetExpenses()).then(()=> {
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
     ReactDOM.render(jsx, document.getElementById('app'));
-});
+    hasRendered = true;
+  }
+};
+
+ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user)=> {
   if (user) {
-    console.log('log in');
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(()=> {
+      renderApp();
+      // to not redirect when you refresh another page while logged in
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
   } else {
-    console.log('log out');
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
   }
 });
 
