07a0ac1c00db517887e4b039ae1ec09f421b670b
Daniele122898 <d.palbanese@gmail.com>
2019-02-09 18:27:29 +0100

    store user data in user area on firebase and only work with authentication

--- Expensify-app/src/actions/expenses.js
+++ Expensify-app/src/actions/expenses.js
@@ -7,8 +7,9 @@ export const removeExpense = (id) => ({
 });
 
 export const startRemoveExpense = (id) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).remove()
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).remove()
       .then(()=> {
         dispatch(removeExpense(id));
       });
@@ -23,8 +24,9 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).update(updates).then(() => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
       dispatch(editExpense(id, updates));
     });
   };
@@ -37,7 +39,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = '',
       note = '',
@@ -45,7 +48,7 @@ export const startAddExpense = (expenseData = {}) => {
       createdAt = 0
     } = expenseData;
     const expense = { description, note, amount, createdAt };
-    database.ref('expenses').push(expense).then((ref)=> {
+    database.ref(`users/${uid}/expenses`).push(expense).then((ref)=> {
       dispatch(addExpense({
         id: ref.key,
         ...expense
@@ -61,9 +64,10 @@ export const setExpenses = (expenses) => ({
 });
 
 export const startSetExpenses = () => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     // to get access of promise in app.js
-    return database.ref('expenses')
+    return database.ref(`users/${uid}/expenses`)
       .once('value')
       .then((snapshot) => {
         const expenses = [];
