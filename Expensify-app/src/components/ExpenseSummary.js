41b34b29fae6c432cfeaf92b093dbc853bfd6ae6
Daniele122898 <d.palbanese@gmail.com>
2019-02-11 19:33:43 +0100

    added hidden expense message

--- Expensify-app/src/components/ExpenseSummary.js
+++ Expensify-app/src/components/ExpenseSummary.js
@@ -7,13 +7,18 @@ import expenseTotal from "../selectors/expenses-total";
 
 numeral.locale('CH');
 
-const ExpenseSummary = ({expenseCount, expensesTotal}) => {
+const ExpenseSummary = ({expenseCount, expensesTotal, unfilteredExpenseCount}) => {
     const expw = expenseCount === 1 ? "expense" : "expenses";
+    const diff = unfilteredExpenseCount - expenseCount;
+    const subexpw = diff === 1 ? "expense" : "expenses";
     const expensesTotalFormatted = numeral(expensesTotal/100).format("$0,0.00");
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expw} totalling <span>{expensesTotalFormatted}</span></h1>
+                {diff !== 0 && (
+                  <h3 className="page-header__subtitle">Not showing {diff} {subexpw} because of filters.</h3>
+                )}
                 <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -26,7 +31,8 @@ const mapStateToProps = (state) => {
     const expenses = selectExpenses(state.expenses, state.filter);
     return {
         expenseCount: expenses.length,
-        expensesTotal: expenseTotal(expenses)
+        expensesTotal: expenseTotal(expenses),
+        unfilteredExpenseCount: state.expenses.length
     }
 };
 
